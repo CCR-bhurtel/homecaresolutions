558041ceb130f949ba12b5ca04f98c51237eaa63
ItsJustIkenna <ikenna.nwagbara@gmail.com>
2021-06-05 12:01:55 -0400

    3rd commit

--- src/components/applicationFormComponent/index.js
+++ src/components/applicationFormComponent/index.js
@@ -2,36 +2,7 @@ import React, { useState, useEffect, makeStyles } from "react";
 import { Grid, TextField } from "@material-ui/core";
 
 function Index() {
-  const initialValues = {
-    id: 0,
-    fullName: "",
-    date: "",
-    address: "",
-    phone: "",
-    email: "",
-    date_available: "",
-    social_security: "",
-    desired_salary: "",
-    position_applied: "",
-    certification: false,
-    citizen: false,
-    authorized: false,
-    ever_worked_for_company: false,
-    ever_convicted: false,
-  };
 
-  const useStyle = makeStyles((theme) => ({
-    root: {
-      "& .MuiFormControl-root": {
-        width: "80%",
-        margin: theme.spacing(1),
-      },
-    },
-  }));
-
-  const [values, setValues] = useState(initialValues);
-
-  const classes = useStyle();
 
   return (
     <>
