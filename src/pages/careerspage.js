cafe959e89834ad66afa3a715ecba820183eacb5
ItsJustIkenna <ikenna.nwagbara@gmail.com>
2021-06-05 11:38:16 -0400

    3rd commit

---
+++ src/pages/careerspage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Applicationform from "../components/applicationFormComponent";
+
+function careerspage() {
+  return (
+    <>
+      <Applicationform />
+    </>
+  );
+}
+
+export default careerspage;
