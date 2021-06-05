a3038760d2988aade95e2f9cd19dc714f0cf9bd5
ItsJustIkenna <ikenna.nwagbara@gmail.com>
2021-06-05 12:15:57 -0400

    3rd commit

--- src/components/applicationFormComponent/index.js
+++ src/components/applicationFormComponent/index.js
@@ -2,25 +2,7 @@ import React, { useState, useEffect, makeStyles } from "react";
 import { Grid, TextField } from "@material-ui/core";
 
 function Index() {
-
-
-  return (
-    <>
-      <form className={classes.root}>
-        <Grid container>
-          <Grid item xs={6}>
-            <TextField
-              variant="outlined"
-              label="Full Name"
-              value={values.fullName}
-            />
-            <TextField variant="outlined" label="date" value={values.date} />
-          </Grid>
-          <Grid item xs={6}></Grid>
-        </Grid>
-      </form>
-    </>
-  );
+  return <></>;
 }
 
 export default Index;
