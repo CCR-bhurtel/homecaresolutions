245db4424f0ecb6eaf33eb4097d8c1e359b18e16
cceer <bhurtelshishir@gmail.com>
2021-07-08 19:37:41 +0545

    all done

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
 const app = express();
-
+app.use(express.static(`${__dirname}/public`))
 app.get('*', (req, res) => {
-  res.sendFile(`${__dirname}/public/index.html`);
+  res.sendFile(`${__dirname}/build/index.html`);
 });
 
 app.listen(process.env.PORT || 5000, () => {
